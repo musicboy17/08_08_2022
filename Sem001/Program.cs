9008d94651c9581979d11ff67921ee32b3cb1298
musicboy17 <pratham@yandex.ru>
2022-08-08 17:02:00 +0300

    Day Number

--- Sem001/Program.cs
+++ Sem001/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // See https://aka.ms/new-console-template for more information
+// // Console.WriteLine("Hello, World!");
+// // See https://aka.ms/new-console-template for more information
+// // Console.WriteLine("Hello, World!");
+// // Напишите программу которая на вход принимает число,
+// // и выдаёт его квадрат (число умноженное на само себя).
+// // 
+// // Например:
+// // 4 -> 16
+// // -3 -> 9
+// // -7 -> 49
+
+// Console.Write("Введите число №1: ");
+
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+
+// Console.Write("Введите число №2: ");
+
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (firstNumber == secondNumber * secondNumber) 
+// {
+//     Console.WriteLine(firstNumber + " является квадратом " + secondNumber);
+
+
+
+// }
+
+// else 
+// {
+//     Console.WriteLine(firstNumber + " НЕ является квадратом " + secondNumber);
+
+// }
+
+// // int square = number * number;
+
+// // Console.WriteLine(square);
+
+// Console.WriteLine("Введите число - номер дня недели");
+// int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (dayNumber == 1)
+// {
+//     System.Console.WriteLine("Понедельник");
+// }
+// if (dayNumber == 2)
+// {
+//     System.Console.WriteLine("Вторник");
+// }
+// if (dayNumber == 3)
+// {
+//     System.Console.WriteLine("Среда");
+// }
+// if (dayNumber == 4)
+// {
+//     System.Console.WriteLine("Четверг");
+// }
+// if (dayNumber == 5)
+// {
+//     System.Console.WriteLine("Пятница");
+// }
+// if (dayNumber == 6)
+// {
+//     System.Console.WriteLine("Суббота");
+// }
+// if (dayNumber == 7)
+// {
+//     System.Console.WriteLine("Воскресенье");
+// }
+
+
